b6fb4811ae5643fe1b63b6b5a2277e86d3198c89
권태욱 <notebook@>
2025-02-23 14:52:47 +0900

    Last

---
+++ src/test/java/com/shop/ShopApplicationTests.java
@@ -0,0 +1,19 @@
+package com.shop;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import com.shop.repository.ItemRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+
+
+class ShopApplicationTests {
+
+
+
+}
